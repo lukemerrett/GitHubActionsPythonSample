c19974446b0a0bc8fb9af202e201efe4571ebf7e
Luke Merrett <lukeamerrett@gmail.com>
2021-05-24 08:34:31 +0100

    Set of basic tests to check the runner is working

---
+++ tests/test_basic.py
@@ -0,0 +1,27 @@
+import pytest
+
+def add(x, y):
+    return x + y
+
+def divide(x, y):
+    if y == 0:
+        raise Exception("Cannot divide by zero")
+    return x / y
+
+def test_should_fail():
+    assert add(1, 2) == 5
+
+def test_should_pass():
+    assert add(1, 2) == 3
+
+def test_should_throw():
+    with pytest.raises(Exception):
+        divide(1, 0)
+
+class TestClass:
+    def test_substring(self):
+        x = "bob"
+        assert "tim" in x
+
+    def test_addition(self):
+        assert add(3, 4) == 7
