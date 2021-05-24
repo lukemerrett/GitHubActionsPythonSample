081a237405edd21305282922f40c644fca9f2b9a
Black Linting <black.linting@example.com>
2021-05-24 07:36:49 +0000

    Linted code

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,17 +4,21 @@ import pytest
 def add(x, y):
     return x + y
 
+
 def divide(x, y):
     if y == 0:
         raise Exception("Cannot divide by zero")
     return x / y
 
+
 def test_should_pass():
     assert add(1, 2) == 3
+
 
 def test_should_throw():
     with pytest.raises(Exception):
         divide(1, 0)
+
 
 class TestClass:
     def test_addition(self):
